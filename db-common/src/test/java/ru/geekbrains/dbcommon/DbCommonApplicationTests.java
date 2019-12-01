2ef32b971e519c3660ec9cb82fd8e773294bef09
Air Mike <mike@mail.ru>
2019-12-01 20:28:07 +0300

    сгенерированы ченджсеты БД версии 1.0

--- db-common/src/test/java/ru/geekbrains/dbcommon/DbCommonApplicationTests.java
+++ db-common/src/test/java/ru/geekbrains/dbcommon/DbCommonApplicationTests.java
@@ -1,15 +1,15 @@
 package ru.geekbrains.dbcommon;
 
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-@SpringBootTest
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//
+//@SpringBootTest
 class DbCommonApplicationTests
 {
 
-  @Test
+  //  @Test
   void contextLoads()
   {
   }
